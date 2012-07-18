cd124db39dd07a56d40d267650d87e1976386d41
Lee <gclark2@gmail.com>
2012-07-18 07:58:19 -0400

    starting icon placement

--- views/list.php
+++ views/list.php
@@ -1,16 +1,19 @@
 <div class="container">
 	<div id="gallery">
 	<?php
-		$galleryArray = $this->model->getGalleryList();
-		foreach($galleryArray as $gallery){
-			echo "<h2>" . $gallery . "</h2>\n<ul>";
+	$galleryArray = $this->model->getGalleryList();
+	foreach($galleryArray as $gallery){
+		echo "\t<h2>" . $gallery . "</h2>
+		<img src=\"xxx.jpg\" height=\"50\" weight=\"50\" alt=\"test\" />
+		<ul>\n";
 
-			$dirArray = $this->model->getAlbumList($gallery);
-			foreach($dirArray as $directory){
-				echo "<li><a href=\"index.php?g=" . base64_encode($gallery ."/" . $directory) . "&t=" . base64_encode($directory) . "\">$directory</a></li>\n";
-			};
+		$dirArray = $this->model->getAlbumList($gallery);
+		foreach($dirArray as $directory){
+			echo "\t\t\t<li><a href=\"index.php?g=" . base64_encode($gallery ."/" . $directory) . "&t=" . base64_encode($directory) . "\">$directory</a></li>\n";
+		};
 
-			echo "</ul>";
+		echo "\t\t</ul>\n
+	";
 		}
 	?>
 	</div>
