b45c96d203aa8bbb2a40685178a9f30588b5c3b7
G. Lee Clark, II <gclark2@gmail.com>
2012-07-18 21:51:40 -0400

    slowly working on gallery icons.

--- views/list.php
+++ views/list.php
@@ -4,12 +4,12 @@
 	$galleryArray = $this->model->getGalleryList();
 	foreach($galleryArray as $gallery){
 		echo "\t<h2>" . $gallery . "</h2>
-		<img src=\"xxx.jpg\" height=\"50\" weight=\"50\" alt=\"test\" />
-		<ul>\n";
+		\n";
 
 		$dirArray = $this->model->getAlbumList($gallery);
 		foreach($dirArray as $directory){
-			echo "\t\t\t<li><a href=\"index.php?g=" . base64_encode($gallery ."/" . $directory) . "&t=" . base64_encode($directory) . "\">$directory</a></li>\n";
+			echo "\t\t\t<li><img src=\"" . GALLERY_ROOT . $gallery ."/" . $directory . "/xxx.jpg\" height=\"50\" weight=\"50\" alt=\"test\" />
+		<ul><a href=\"index.php?g=" . base64_encode($gallery ."/" . $directory) . "&t=" . base64_encode($directory) . "\">$directory</a></li>\n";
 		};
 
 		echo "\t\t</ul>\n
